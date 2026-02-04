d6ef8b87df5bf098af09c27307673dae51ec08bc
nischalkafle66-dev <nischalkafle66@gmail.com>
2026-02-04 17:23:46 +0545

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,8 @@
+Unit:1 (problem solving with computer)
+write a program to calculate simple interset
+using problem analysis
+
+inputs:principal,time,rate
+output:simple interest
+formula:SI=(p*t*+r)/100
+steps:read inputs->calculate si ->print result
